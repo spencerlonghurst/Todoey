5e6c59d84d19f012814d2fd1393f86681576f491
Spencer Longhurst <spencersean12@gmail.com>
2023-05-15 21:42:59 -0600

    added hardcoded values to set up the tableview and the cells

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -9,12 +9,35 @@
 import UIKit
 
 class TodoListViewController: UITableViewController {
+    
+    let itemArray = ["find Mike", "Buy Eggos", "Destroy Demogorgon"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithTransparentBackground()
+        appearance.backgroundColor = UIColor.systemBlue
+        appearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+        navigationItem.standardAppearance = appearance
+        navigationItem.scrollEdgeAppearance = appearance
+    }
+    
+    //MARK - Tableview Datsource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
     }
 
+    
 
 }
 
